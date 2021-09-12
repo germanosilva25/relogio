92b6f269fcd2f4beceb4452d99fa1f47f87dc297
germanosilva25 <germanosilva@yahoo.com.br>
2021-09-12 19:55:22 -0300

    Segundo

--- index.php
+++ index.php
@@ -8,6 +8,13 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <style rel = "stylesheet">
+      .horas{
+          font-color: blue;
+      }
+      </style>
+
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <script>
   	function increment(){
   		//alert('testye')
@@ -15,22 +22,25 @@
   	}
 
   	function deletar(){
-  		$('table').find('tbody').find('tr:last').remove()
+  		$('table').find('tbody').find('tr').remove()
   	}
 
 
 function parar(){
 
 }
+let intervalCentesimos = '';
+let intervalSegundos = '';
+let s = 0;
+let m = 0;
+let h = 0;
+let c = 0;
+let reniciar = 0;
+let volta = 0;
 function iniciar(){
-   let s = 0;
-   let m = 0;
-   let h = 0;
-   let c = 0;
-
    
-
-   let intervalCentesimos = setInterval(function(){
+   let zerar = 0;
+    intervalCentesimos = setInterval(function(){
        c += 1; 
        if(c == 100){
            c = 0;
@@ -41,7 +51,7 @@ function iniciar(){
        
     },1);
 
-    let intervalSegundos = setInterval(function(){
+    intervalSegundos = setInterval(function(){
        s += 1; 
        
        if (s == 60){
@@ -57,78 +67,88 @@ function iniciar(){
        $(".minutos").html(String(m).padStart(2, "0"));
        $(".horas").html(String(h).padStart(2, "0"));
     },1000);
-    
-    return function parcial(){
-       alert('segundo: ' + s)
-   }
 
-    return function zerar(){
-        alert('interval: ')
-       //clearInterval(intervalCentesimos);
-       //clearInterval(intervalSegundos); 
-   }
+    };
 
-   
-   };
+    function zerar(){
+       
+        c = 0;
+        s = 0;
+        m = 0;
+        h = 0;
+        clearInterval(intervalSegundos);
+        clearInterval(intervalCentesimos);
+        $(".centesimos").html(String(c).padStart(2, "0"));
+        $(".segundos").html(String(s).padStart(2, "0"));
+        $(".minutos").html(String(m).padStart(2, "0"));
+        $(".horas").html(String(h).padStart(2, "0"));
+        $('table').find('tbody').find('tr').remove()
+    }
+
+    function parcial(){
+        volta += 1
+        console.log(h + ':' + m + ':' + s + ':' + c)
+        $('table').find('tbody').append("<tr><td>" + volta + ": " + String(h).padStart(2, "0") + ":" + String(m).padStart(2, "0") + ":" + String(s).padStart(2, "0") + ":" + String(c).padStart(2, "0") + "</td></tr>")
+    }
+
+    function parar(){
+        //reniciar = 1;
+        clearInterval(intervalSegundos)
+        clearInterval(intervalCentesimos)
+        if(reniciar == 0){
+            $('#parar').html('Reiniciar')
+            reniciar = 1;
+        }else{
+            reniciar = 0;
+            $('#parar').html('Parar')
+            iniciar()
+        }
+        
+
+    }
   </script>
 </head>
 <body>
 
 <div class="container">
-  <h1>Hora: </h1>
+  <h1>Cronômetro </h1>
   <p>
- <span class = "horas colored digit text-nowrap font-digit">00</span>
+ <span class = "horas w3-text-blue w3-sans-serif">00</span>
   <span>:</span>
- <span class = "minutos colored digit text-nowrap font-digit">00</span>
+ <span class = "minutos w3-text-blue">00</span>
  <span>:</span>
- <span class = "segundos colored digit text-nowrap font-digit">00</span>
+ <span class = "segundos w3-text-blue w3-sans-serif">00</span>
  <span>:</span>
- <span class = "centesimos colored digit text-nowrap font-digit">00</span>
+ <span class = "centesimos w3-text-blue">00</span>
  <span></span>
 </p>
 <p>
  <button type="button" class="btn btn-primary" onclick = "iniciar()">Inicar</button>
  <span></span>
- 
- <button type="button" class="btn btn-info" onclick = "iniciar()()">Parcial</button>
+ <button type="button" class="btn btn-info" onclick = "parcial()">Parcial</button>
  <span></span>
- <button type="button" class="btn btn-danger" onclick = "iniciar()()">Zerar</button>
+ <button type="button" id = "parar" class="btn parar btn-warning" onclick = "parar()">Parar</button>
+ <span></span>
+ <button type="button" class="btn btn-danger" onclick = "zerar()">Zerar</button>
 
 </p>
 </div>
 
 <div class="container">
-  <h2>Basic Table</h2>
+  <h2>Tabela dos valores parciais</h2>
              
   <table class="table">
     <thead>
       <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
+        <th>Parciais</th>
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-      </tr>
+      
     </tbody>
   </table>
 </div>
-<button type="button" class="btn btn-primary" onclick = "increment()">Primary</button>
-<button type="button" class="btn btn-danger" onclick = "deletar()">Danger</button>
+
 </body>
 </html>
 
